7c71b98254bc112dff9fb31eeba078d594dec9c5
NikFel <stena-55@mail.ru>
2018-10-12 20:45:02 +0500

    Add page Movies

--- RazorPagesMovie/Startup.cs
+++ RazorPagesMovie/Startup.cs
@@ -1,9 +1,11 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
 
 namespace RazorPagesMovie
 {
@@ -28,6 +30,9 @@ namespace RazorPagesMovie
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<RazorPagesMovieContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("RazorPagesMovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
